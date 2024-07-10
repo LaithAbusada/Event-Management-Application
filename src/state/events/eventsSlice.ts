a6049a459cb5dbbbeeba7dfd45fcbe3df3103927
Laith Abusada <lai20200812@std.psut.edu.jo>
2024-07-10 12:46:24 +0300

    Firebase Listener: Added firebase real time listener and integrated it with navbar

---
+++ src/state/events/eventsSlice.ts
@@ -0,0 +1,20 @@
+import { EventState } from "@/interfaces/EventInterface";
+import { createSlice } from "@reduxjs/toolkit";
+import { getEvents } from "@/lib/firebase/firestore";
+
+const initialState: EventState = {
+  data: null,
+};
+
+const eventSlice = createSlice({
+  name: "events",
+  initialState,
+  reducers: {
+    setEvents(state, action) {
+      state.data = action.payload;
+    },
+  },
+});
+
+export const { setEvents } = eventSlice.actions;
+export default eventSlice.reducer;
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { config } from "dotenv";
 import authReducer from "./user/authSlice";
-
+import eventsReducer from "./events/eventsSlice";
+// redux store
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    events: eventsReducer,
   },
 });
 
